7ffc0fcc2d24b1679743a3a5adcbed5a002f5c25
Xanthe Sharp <67866631+Xanthe82@users.noreply.github.com>
2022-11-27 16:55:19 +0000

    Add files via upload

---
+++ environment/categorize-values.py
@@ -0,0 +1,3 @@
+myMixedTypeList=[45, False, "hi there", 2009.34]
+for item in myMixedTypeList:
+ print(f"{item} is of the data type {type(item)}")
---
+++ environment/collections.py
@@ -0,0 +1,26 @@
+#Lists
+
+myFruitList=["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+print(myFruitList[0])
+myFruitList[2]="orange"
+print(myFruitList)
+
+#Tuples
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[1])
+
+#Dictionaries
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+print(myFavoriteFruitDictionary["Saanvi"])
